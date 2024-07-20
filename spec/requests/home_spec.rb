d64599404a1e8c5da2ac6a64b6a1c668c1c2777f
DESKTOP-PU8A88A\kawan <test>
2024-07-20 15:45:58 +0900

    Home#toのテストを変更

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Homes', type: :request do
-  describe 'GET /top' do
+  describe 'GET /' do
     it 'returns http success' do
-      get '/home/top'
+      get '/'
       expect(response).to have_http_status(:success)
     end
   end
